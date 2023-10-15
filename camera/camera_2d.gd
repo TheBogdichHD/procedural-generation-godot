2ecc9b99f92fb98dfeb70c4e9ec692d9a89320db
TheBogdichHD <nottoowellconstructed@gmail.com>
2023-10-15 22:16:33 +0300

    Camera updated

--- camera/camera_2d.gd
+++ camera/camera_2d.gd
@@ -12,32 +12,20 @@ func _process(_delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		if Input.is_action_pressed("right_click"):
+		if Input.is_action_pressed("right_click") or Input.is_action_pressed("middle_click"):
 			position -= event.relative / zoom.x
-
-	if event.is_action_pressed("scroll_up"):
-		var new_zoom = zoom * (1 + zoom_speed)
-		
-		if new_zoom.x < zoom_upper_bound:
-			var previous_mouse_position = get_local_mouse_position()
-			zoom = new_zoom
-			var diff = previous_mouse_position - get_local_mouse_position()
-			offset += diff
 	
+	if event.is_action_pressed("scroll_up"):
+		zoom_camera(1)
 	elif event.is_action_pressed("scroll_down"):
-		var new_zoom = zoom * (1 - zoom_speed)
-		
-		if new_zoom.x > zoom_lower_bound:
-			var previous_mouse_position = get_local_mouse_position()
-			zoom = new_zoom
-			var diff = previous_mouse_position - get_local_mouse_position()
-			offset += diff
+		zoom_camera(-1)
 
-#func zoom_camera(direction: int) -> void:
-#	var new_zoom = get_zoom() + Vector2(scroll_speed, scroll_speed) * direction
-#
-#	if new_zoom.x > zoom_lower_bound:
-#		var previous_mouse_position = get_local_mouse_position()
-#		zoom = new_zoom
-#		var diff = previous_mouse_position - get_local_mouse_position()
-#		offset += diff
+
+func zoom_camera(direction: int) -> void:
+	var new_zoom = zoom * (1 + zoom_speed * direction)
+	
+	if new_zoom.x > zoom_lower_bound and new_zoom.x < zoom_upper_bound:
+		var previous_mouse_position = get_local_mouse_position()
+		zoom = new_zoom
+		var diff = previous_mouse_position - get_local_mouse_position()
+		offset += diff
